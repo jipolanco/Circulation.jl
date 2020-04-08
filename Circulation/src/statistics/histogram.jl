48ce4671ce7ce2ee650db2e42850c2b8fc6fc25d
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-04-08 19:23:11 +0200

    Compute min and max sampled values

--- Circulation/src/statistics/histogram.jl
+++ Circulation/src/statistics/histogram.jl
@@ -3,11 +3,14 @@
 
 Histogram of a scalar quantity.
 """
-struct Histogram{T, BinType<:AbstractVector}
+struct Histogram{T, Tb, BinType<:AbstractVector{Tb}}
     Nr    :: Int          # number of "columns" of data (e.g. one per loop size)
     Nbins :: Int          # number of bins
-    bin_edges :: BinType  # sorted list of bin edges (length Nbins + 1)
-    H :: Matrix{T}        # histogram (Nbins, Nr)
+    bin_edges :: BinType  # sorted list of bin edges [Nbins + 1]
+    H :: Matrix{T}        # histogram [Nbins, Nr]
+
+    vmin :: Vector{Tb}    # minimum sampled value [Nr]
+    vmax :: Vector{Tb}    # maximum sampled value [Nr]
 
     # Number of samples per column (Nr).
     # This includes outliers, i.e. events falling outside of the histogram.
@@ -17,9 +20,12 @@ struct Histogram{T, BinType<:AbstractVector}
                        ::Type{T} = Int) where {T}
         Nbins = length(bin_edges) - 1
         H = zeros(T, Nbins, Nr)
-        BinType = typeof(bin_edges)
         Nsamples = zeros(Int, Nr)
-        new{T, BinType}(Nr, Nbins, bin_edges, H, Nsamples)
+        BinType = typeof(bin_edges)
+        Tb = eltype(bin_edges)
+        vmin = zeros(Tb, Nr)
+        vmax = zeros(Tb, Nr)
+        new{T, Tb, BinType}(Nr, Nbins, bin_edges, H, vmin, vmax, Nsamples)
     end
 end
 
@@ -32,12 +38,23 @@ function update!(s::Histogram, Γ, r)
     s.Nsamples[r] += length(Γ)
     Ne = length(s.bin_edges)
 
+    vmin = s.vmin[r]
+    vmax = s.vmax[r]
+
     @inbounds for v in Γ
+        # The "oftype" is just to make sure that `vmin` doesn't change type
+        # (e.g. from Float64 to Float32, in case `v` is the latter), which would
+        # be very bad for performance.
+        vmin = min(oftype(vmin, v), vmin)
+        vmax = max(oftype(vmax, v), vmax)
         i = searchsortedlast(s.bin_edges, v)
         if i ∉ (0, Ne)
             s.H[i, r] += 1
         end
     end
+
+    s.vmin[r] = vmin
+    s.vmax[r] = vmax
 
     s
 end
@@ -49,6 +66,8 @@ function reduce!(s::Histogram, v::AbstractVector{<:Histogram})
         @assert s.bin_edges == src.bin_edges
         s.H .+= src.H
         s.Nsamples .+= src.Nsamples
+        s.vmin .= min.(s.vmin, src.vmin)
+        s.vmax .= max.(s.vmax, src.vmax)
     end
     s
 end
@@ -58,12 +77,17 @@ finalise!(s::Histogram) = s  # nothing to do
 function reset!(s::Histogram)
     fill!(s.H, 0)
     fill!(s.Nsamples, 0)
+    fill!(s.vmin, 0)
+    fill!(s.vmax, 0)
     s
 end
 
 function Base.write(g, s::Histogram)
     g["bin_edges"] = collect(s.bin_edges)
     g["total_samples"] = s.Nsamples
+
+    g["minimum"] = s.vmin
+    g["maximum"] = s.vmax
 
     # Write compressed histogram (compression ratio can be huge!)
     let Nbins = size(s.H, 1)
